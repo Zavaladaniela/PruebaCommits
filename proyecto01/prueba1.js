4ba65004fa02d3c16ce0d8cf8bb26b76d641800e
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-19 22:50:13 -0500

    Add files via upload
    Agregue mi carpeta previamente versiona con git

---
+++ proyecto01/prueba1.js
@@ -0,0 +1,11 @@
+const Puffy = {
+    edad: "8 años",
+    color: "blanco con manchas cafes",
+    actividad: "le gusta dormir",
+    hijos: "2",
+    operaciones: "1",
+    ropa: "prefiere no usar",
+    baños: "3 al año"
+}
+
+console.log (Puffy)
